769729beceb88518805243903a7b20e6fab266eb
Francisco Netto <franciscon238@gmail.com>
2022-08-10 22:23:26 -0300

    basic structure 2

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,12 +1,31 @@
 import styled from "styled-components";
 
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    width: 100%;
+    max-width: 750px;
+    margin: auto;
+    display: flex;
+    padding: 50px 0;
+`;
 
-export const Info = styled.div``;
+export const Info = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: auto;
+`;
 
-export const LogoLink = styled.a``;
+export const LogoLink = styled.a`
+    display: block;
+`;
 
-export const InfoArea = styled.div``;
+export const InfoArea = styled.div`
+    width: 100%;
+    margin: 10px 0;
+`;
 
-export const GridArea = styled.div``;
+export const GridArea = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+`;
