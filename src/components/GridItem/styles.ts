03b44c5734942f7944a236624505a68f86252388
Francisco Netto <franciscon238@gmail.com>
2022-08-14 23:38:30 -0300

    display on screen

--- src/components/GridItem/styles.ts
+++ src/components/GridItem/styles.ts
@@ -1,7 +1,25 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    background-color: purple;
+type ContainerProps = {
+    showBackground: boolean
+}
+
+export const Container = styled.div<ContainerProps>`
+    background-color: ${props => props.showBackground ? '#1550FF' : '#E2E3E3'};
+    height: 100px;
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
 `;
 
-export const Icon = styled.img``;
+type IconProps = {
+    opacity?: number;
+}
+
+export const Icon = styled.img<IconProps>`
+    height: 40px;
+    width: 40px;
+    opacity: ${props => props.opacity ?? 1};
+`;
